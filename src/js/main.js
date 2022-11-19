5ba75b2c77a3186e249bd42fb55b3b21c5bbeb51
Manuel Augusto Bravard <manubravard@gmail.com>
2022-11-19 00:22:43 -0300

    some notes

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
+//this won't work (individually) because we need to instantiate a Popover.
+// import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+
+//we need to import the class and instantiate a Popover
 // Import only the Bootstrap components we need
 import { Util, Dropdown, Offcanvas, Popover } from 'bootstrap';
 
