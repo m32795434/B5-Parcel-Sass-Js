98bdd07a45d50089ffaa43f43d4e6de7b4225477
Manuel Augusto Bravard <manubravard@gmail.com>
2022-11-19 16:50:55 -0300

    creating a popover

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
-import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import bootstrap from '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+const popers = document.querySelectorAll('[data-bs-toggle="popover"]');
+Array.from(popers).forEach((pop)=>{
+    new bootstrap.Popover(pop)
+})
