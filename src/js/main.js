3fd3d6c9c3afddfa104658c26f4bb18b52e16c6e
Manuel Augusto Bravard <manubravard@gmail.com>
2022-11-19 12:55:33 -0300

    new branch to practice

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,1 @@
-//this won't work (individually) because we need to instantiate a Popover.
-// import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
-
-//we need to import the class and instantiate a Popover
-// Import only the Bootstrap components we need
-import { Util, Dropdown, Offcanvas, Popover } from 'bootstrap';
-
-// Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-  .forEach(popover => {
-    new Popover(popover)
-  })
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
