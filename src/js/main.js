a0873411c68a261b1142fd016dff92879f5a2b51
Manuel Augusto Bravard <manubravard@gmail.com>
2022-11-21 11:50:28 -0300

    working on buttons, anchors, grid system, color variables, popover

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
-import bootstrap from '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+/*import bootstrap from '../../node_modules/bootstrap/dist/js/bootstrap.min';//.js too
+const popers = document.querySelectorAll('[data-bs-toggle="popover"]');
+Array.from(popers).forEach((pop)=>new bootstrap.Popover(pop));
+*/
+
+import popover from '../../node_modules/bootstrap/js/dist/popover.js';
+const popers = document.querySelectorAll('[data-bs-toggle="popover"]');
+Array.from(popers).forEach((pop)=>new popover(pop));
+
+
+/*
+import {Popover} from 'bootstrap';
 const popers = document.querySelectorAll('[data-bs-toggle="popover"]');
 Array.from(popers).forEach((pop)=>{
-    new bootstrap.Popover(pop)
+    new Popover(pop)
 })
+*/
