332f7db670df222d23434adf0024ff358e1a190d
Manuel Augusto Bravard <manubravard@gmail.com>
2022-11-21 22:32:01 -0300

    turning grid to flex for some responsive divs. Button active toggle

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 const popers = document.querySelectorAll('[data-bs-toggle="popover"]');
 Array.from(popers).forEach((pop)=>new bootstrap.Popover(pop));
 */
+import '../../node_modules/bootstrap/js/dist/button';
 
 import popover from '../../node_modules/bootstrap/js/dist/popover.js';
 const popers = document.querySelectorAll('[data-bs-toggle="popover"]');
