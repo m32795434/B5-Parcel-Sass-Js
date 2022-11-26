86c9bb78fdf834803bfd3797b8c14754b9be556b
Manuel Augusto Bravard <manubravard@gmail.com>
2022-11-26 01:37:41 -0300

    boots dropdown customized (made at home)

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@
 const popers = document.querySelectorAll('[data-bs-toggle="popover"]');
 Array.from(popers).forEach((pop)=>new bootstrap.Popover(pop));
 */
+import '../../node_modules/@popperjs/core';
+import '../../node_modules/bootstrap/js/dist/dropdown';
 import '../../node_modules/bootstrap/js/dist/button';
 
 import popover from '../../node_modules/bootstrap/js/dist/popover.js';
